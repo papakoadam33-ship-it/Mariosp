580d82c8f9514e30dc162caa7fa2ec243855e52a
papakoadam33-ship-it <papakoadam33@gmail.com>
2026-03-19 22:51:46 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
+import requests
+import sqlite3
+import os
+
+API_KEY = os.environ.get('FOOTBALL_API_KEY')
+BASE_URL = "https://api.football-data.org/v4/competitions/PL/matches"
+
+def fetch_data():
+    headers = {'X-Auth-Token': API_KEY}
+    response = requests.get(BASE_URL, headers=headers)
+    if response.status_code == 200:
+        print("✅ Επιτυχία! Το API απάντησε.")
+    else:
+        print(f"❌ Σφάλμα: {response.status_code}")
+
+if __name__ == "__main__":
+    fetch_data()
 
